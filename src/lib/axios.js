1110b531c84d071138b357974e9f030c38079eec
Dhwarakesh <Dhwarakesh31@gmail.com>
2024-10-28 18:43:04 -0400

    updated the end point

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:4000/api" : "http://3.80.74.141/api",
+    baseURL: import.meta.mode === "development" ? "http://localhost:4000/api" : "http://100.27.195.186/api",
     withCredentials: true,
 });
 
