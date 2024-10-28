2f2ce328981c6cec19f9f228527969ddfae0328e
Dhwarakesh <Dhwarakesh31@gmail.com>
2024-10-28 18:19:58 -0400

    updated api endpoint

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL : import.meta.mode === "development" ? "http://localhost:4000/api" : "/api",
-    withCredentials : true, // send cookies to the server
- });
+    baseURL: import.meta.mode === "development" ? "http://localhost:4000/api" : "http://3.80.74.141/api",
+    withCredentials: true,
+});
 
 
  export default axiosInstance;
