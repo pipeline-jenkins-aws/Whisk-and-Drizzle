d8219d6f40ad3a99d923a57f0a52d8694e92175d
Dhwarakesh <Dhwarakesh31@gmail.com>
2024-10-28 18:59:12 -0400

    updated the end point

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-
 const axiosInstance = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:4000/api" : "http://100.27.195.186/api",
-    withCredentials: true,
+    baseURL: "http://100.27.195.186/api", // Always points to the production backend
+    withCredentials: true, // Ensures cookies are sent with requests
 });
 
-
- export default axiosInstance;
+export default axiosInstance;
