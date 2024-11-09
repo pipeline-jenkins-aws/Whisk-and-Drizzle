8cee7885ea470eec24e4386767b88f22d1a4d3e3
Dhwarakesh95 <82435431+Dhwarakesh95@users.noreply.github.com>
2024-11-09 09:52:18 -0500

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "http://54.226.214.191/api",
     withCredentials: true,
 });
 
